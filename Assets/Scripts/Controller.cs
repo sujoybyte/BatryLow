893b103485f8b1c3f0c03c66dcadc0c8f3e6cadd
Sujoy Ghosh <sujoyself@gmail.com>
2020-04-20 06:43:30 +0530

    add controller script behaviour

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField] private float jumpSpeed = 8f;
+    [SerializeField] private float moveSpeed = 5f;
+    private SpriteRenderer playerSprite = null;
+
+    void Start()
+    {
+        playerSprite = GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpSpeed;
+        
+        float moveDirection = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector2(moveDirection * Time.fixedDeltaTime * moveSpeed, 0f));
+
+        if (moveDirection < 0) playerSprite.flipX = true;
+        else if (moveDirection > 0) playerSprite.flipX = false;
+    }
+}
