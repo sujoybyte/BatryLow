51f27c7e1f7b69e6d7df3618fd8c92862d6bffdd
Sujoy Ghosh <sujoyself@gmail.com>
2020-04-20 08:41:32 +0530

    add platform check while jumping

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -4,24 +4,44 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    [SerializeField] private float jumpSpeed = 8f;
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpSpeed = 8f, moveSpeed = 5f;
     private SpriteRenderer playerSprite = null;
+    private bool playerOnGround;
+    [SerializeField] private Transform playerFeet = null;
+    public LayerMask overlapingLayer;
+    private float maxJump = 2;
 
     void Start()
     {
         playerSprite = GetComponent<SpriteRenderer>();
     }
 
-    void FixedUpdate()
+    private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-            GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpSpeed;
-        
+        // player move and face to the left or right
         float moveDirection = Input.GetAxis("Horizontal");
-        transform.Translate(new Vector2(moveDirection * Time.fixedDeltaTime * moveSpeed, 0f));
+        if (Input.GetKeyDown(KeyCode.A) || moveDirection < 0)
+        {
+            transform.Translate(new Vector2(moveDirection * Time.deltaTime * moveSpeed, 0f));
+            playerSprite.flipX = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || moveDirection > 0)
+        {
+            transform.Translate(new Vector2(moveDirection * Time.deltaTime * moveSpeed, 0f));
+            playerSprite.flipX = false;
+        }
+    }
 
-        if (moveDirection < 0) playerSprite.flipX = true;
-        else if (moveDirection > 0) playerSprite.flipX = false;
+    private void FixedUpdate()
+    {
+        playerOnGround = Physics2D.OverlapCircle(playerFeet.position, 0.4f, overlapingLayer);
+        if (playerOnGround) maxJump = 2;
+
+        // player jump
+        if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && maxJump > 0)
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpSpeed;
+            maxJump--;
+        }
     }
 }
